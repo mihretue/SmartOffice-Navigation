ab7b6ad5fcf7894e36f0d1511ccbf56cbed28b0c
BEKANCY7 <berekettegene4@gmail.com>
2025-03-07 09:15:21 +0300

    minor

--- smart_office_navigation/src/pages/Navigation.tsx
+++ smart_office_navigation/src/pages/Navigation.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
           <div className="relative cursor-pointer" onClick={toggleCard}>
             <Bell className="w-6 h-6 text-gray-500 hover:text-[#009688]" />
             <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              3
+              1
             </span>
           </div>
 
