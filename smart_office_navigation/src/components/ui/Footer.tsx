e93569dcc828050d8ef72fea4091996783354a55
BEKANCY7 <berekettegene4@gmail.com>
2025-03-17 15:47:00 +0300

    minor change

--- smart_office_navigation/src/components/ui/Footer.tsx
+++ smart_office_navigation/src/components/ui/Footer.tsx
@@ -1,15 +1,17 @@
+import { FaEnvelope, FaPhone, FaMapMarker } from 'react-icons/fa';
+
 const Footer = () => {
     return (
         <footer className="fixed bottom-0 left-0 right-0 h-auto bg-[#009688] text-center py-4">
             <div className="flex justify-around items-center">
                 <div className="text-white">
-                    <p>አይቲዲቢ - የቅጂ መብት © {new Date().getFullYear()} 2024 ሁሉም መብቶች የተጠበቁ ናቸው - በ አይቲዲቢ የበለጸገ</p>
+                    <p>አይቲዲቢ - የቅጂ መብት © {new Date().getFullYear()} ሁሉም መብቶች የተጠበቁ ናቸው - በ አይቲዲቢ የበለጸገ</p>
                 </div>
                 <div className="text-white">
-                    <p>ስለኛ ማህደሮች</p>
-                    <p><i className="fas fa-envelope"></i> tradebureau4541@gmail.com</p>
-                    <p><i className="fas fa-phone"></i> 011 111 5581</p>
-                    <p><i className="fas fa-phone"></i> Arada Subcity Woreda 1</p>
+
+                    <p><FaEnvelope className="inline-block mr-2" /> tradebureau4541@gmail.com</p>
+                    <p><FaPhone className="inline-block mr-2" /> 011 111 5581</p>
+                    <p><FaMapMarker className="inline-block mr-2" /> Arada Subcity Woreda 1</p>
                     <p> Wastina Commercial Center, 13th and 14th Floor</p>
                 </div>
             </div>
